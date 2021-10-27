3fb6edbcbc1c065d7fa4ffae001c2bf1ddeb12e5
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-10-27 19:21:07 +0200

    useDocument checks for invalid document ID's

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -5,12 +5,18 @@ export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null);
   const [error, setError] = useState(null);
 
+  // GET REALTIME DATA
   useEffect(() => {
     const collectionRef = projectFirestore.collection(collection).doc(id);
 
     const unSubscribe = collectionRef.onSnapshot((snapshot) => {
-      setDocument({...snapshot.data(), id: snapshot.id });
-      setError(null);
+      // CHECK IF DOCUMENT EXISTS IN DB
+      if (snapshot.data()) {
+        setDocument({...snapshot.data(), id: snapshot.id });
+        setError(null);
+      } else {
+        setError('No such document in database');
+      }
     }, (err) => {
       console.log(err.message);
       setError('document fetch failed');
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
+
+// CSS STYLES
 import './Project.css';
 
 export default function Project() {
+  const { id } = useParams();
+  const { error, document } = useDocument('projects', id);
+
+  // CHECK FOR ERRORS
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+
+  // CHECK FOR NO DOCUMENT
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>
-      Project
+    <div className="project-details">
+      <h1>{document.name}</h1>
     </div>
   )
 }
